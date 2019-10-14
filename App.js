67e254213d6219962a85ae58a36773f4b7dae5bb
Alejandro Gonzales <jandog8990@gmail.com>
2019-10-14 00:00:57 -0600

    Added new list view images with book uris

--- App.js
+++ App.js
@@ -28,6 +28,9 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+// Import another project file
+//import List from './components/List'
+
 //const App: () => React$Node = () => {
 export default class UnstatedApp extends Component {
   
@@ -60,6 +63,13 @@ export default class UnstatedApp extends Component {
 	this.fetchJSONAsync(url);
   }
 
+  // Render item function
+  _renderItem(item) {
+    return ( 
+	  <Image style={{width: 120, height: 180, marginRight: 5, marginTop: 5}} source={{uri: item.PHOTO_LOC}}/> 
+	)	
+  }
+
   render() {
 	  let pic = {
         uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
@@ -73,13 +83,16 @@ export default class UnstatedApp extends Component {
 		)
 	  }
 
+	  //renderItem={({item}) => <Text>{item.TITLE}, {item.AUTHOR}</Text>}
+	  //horizontal	
+	  //SeparatorComponent={() => <View style={{marginRight: 5}} />}	
 	  return (
 		<View style={{alignItems: 'center', top: 50}}>
 		<View style={styles.sectionContainer}>
 		  <FlatList
-			data={this.state.dataSource}
-			renderItem={({item}) => <Text>{item.TITLE}, {item.AUTHOR}</Text>}
+			renderItem={({item}) => this._renderItem(item)} 
 			keyExtractor={({id}, index) => id}
+			data={this.state.dataSource}
 		  />
 		</View>
 		</View>
@@ -124,88 +137,6 @@ export default class UnstatedApp extends Component {
 	  });
  	*/ 
 
-/*
-		  <SafeAreaView>
-			<ScrollView
-			  contentInsetAdjustmentBehavior="automatic"
-			  style={styles.scrollView}>
-			  <Header />
-			  {global.HermesInternal == null ? null : (
-				<View style={styles.engine}>
-				  <Text style={styles.footer}>Engine: Hermes</Text>
-				</View>
-			  )}
-			  <View style={{alignItems: 'center', top: 50}}>
-				<View style={styles.sectionContainer}>
-				  <Text style={styles.sectionTitle}>Step One</Text>
-				  <Text style={styles.sectionDescription}>
-					Edit <Text style={styles.highlight}>App.js</Text> to change this
-					screen and then come back to see your edits.
-				  </Text>
-				</View>
-				<View style={styles.sectionContainer}>
-				  <Text style={styles.sectionTitle}>See Your Changes</Text>
-				  <Text style={styles.sectionDescription}>
-					<ReloadInstructions />
-				  </Text>
-				</View>
-				<View style={styles.sectionContainer}>
-				  <Text style={styles.sectionTitle}>Debug</Text>
-				  <Text style={styles.sectionDescription}>
-					<DebugInstructions />
-				  </Text>
-				</View>
-				<View style={styles.sectionContainer}>
-				  <Text style={styles.sectionTitle}>Learn More</Text>
-				  <Text style={styles.sectionDescription}>
-					Read the docs to discover what to do next:
-				  </Text>
-				</View>
-				<LearnMoreLinks />
-			  </View>
-			</ScrollView>
-		  </SafeAreaView>
-*/
-
-/*
-class Blink extends Component {
-
-  componentDidMount(){
-    // Toggle the state every second
-    setInterval(() => (
-      this.setState(previousState => (
-        { isShowingText: !previousState.isShowingText }
-      ))
-    ), 1000);
-  }
-
-  //state object
-  state = { isShowingText: true };
-
-  render() {
-    if (!this.state.isShowingText) {
-      return null;
-    }
-
-    return (
-      <Text>{this.props.text}</Text>
-    );
-  }
-}
-*/
-
-/*
-class Greeting extends Component {
-  render() {
-    return (
-      <View style={{alignItems: 'center'}}>
-        <Text>Hello {this.props.name}!</Text>
-      </View> 
-    );
-  }
-}
-*/
-
 const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: Colors.lighter,
@@ -244,20 +175,3 @@ const styles = StyleSheet.create({
     textAlign: 'right',
   },
 });
-
-//export default App;
-/*
-<View style={{alignItems: 'center', top: 50}}>
-  <Greeting name='Rexxar' />
-  <Greeting name='Jaina' />
-  <Greeting name='Valeera' />
-</View>
-*/	
-/*	
-<View>
-  <Blink text='I love to blink' />
-  <Blink text='Yes blinking is so great' />
-  <Blink text='Why did they ever take this out of HTML' />
-  <Blink text='Look at me look at me look at me' />
-</View>	
-*/	
